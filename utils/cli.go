4348f9bbb2f8239e61b33b407d456b49ffaf6447
en-vee <neeraj.vaidya@yahoo.co.in>
2019-04-22 17:03:12 +1000

    updated config file

--- utils/cli.go
+++ utils/cli.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 	"time"
 
 	"github.com/en-vee/alog"
@@ -11,7 +12,7 @@ import (
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:10080", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:2080", grpc.WithInsecure())
 	if err != nil {
 		alog.Critical("Error dialing GRPC server : %v", err)
 	}
@@ -24,6 +25,7 @@ func main() {
 	resp, err := client.CreateCustomer(ctx, &provisioning.CreateCustomerRequest{CustomerId: "C12345"})
 	if err != nil {
 		alog.Critical("Error sending GRPC request : %v", err)
+		os.Exit(1)
 	}
 
 	alog.Info("Response = %v", resp)
