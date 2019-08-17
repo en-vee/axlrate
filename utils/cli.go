33ffb9d86ab677391ba1474f5c4f1da3547cffc2
en-vee <neeraj.vaidya@yahoo.co.in>
2019-08-17 21:59:50 +1000

    axlrate.conf : updated address to localhost

--- utils/cli.go
+++ utils/cli.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:2080", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:10080", grpc.WithInsecure())
 	if err != nil {
 		alog.Critical("Error dialing GRPC server : %v", err)
 	}
